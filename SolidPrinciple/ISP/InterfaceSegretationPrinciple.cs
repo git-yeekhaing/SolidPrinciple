11b3d2e3f73faa77afc01ee4718bfe63f2baab92
git-yeekhaing <yee.khaing.git@gmail.com>
2020-07-02 18:22:31 +0900

    using interface segregation principle

---
+++ SolidPrinciple/ISP/InterfaceSegretationPrinciple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciple.ISP
+{
+    class InterfaceSegretationPrinciple
+    {
+    }
+
+    public interface IPrinterTasks2
+    {
+        void Print(string PrintContent);
+        void Scan(string ScanContent);
+    }
+
+    interface IFaxTasks
+    {
+        void Fax(string content);
+    }
+    interface IPrintDuplexTasks
+    {
+        void PrintDuplex(string content);
+    }
+}
