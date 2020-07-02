18bcc770d874479b5ab6c52246a8b7939dc5d8b3
git-yeekhaing <yee.khaing.git@gmail.com>
2020-07-02 14:00:38 +0900

    without using single responsibility principle

---
+++ SolidPrinciple/Invoice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciple
+{
+    public class Invoice
+    {
+        public long InvAmount { get; set; }
+        public DateTime InvDate { get; set; }
+
+        public void AddInvoice()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"c:\ErrorLog.txt", ex.ToString());
+            }
+        }
+
+        public void DeleteInvoice()
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"c:\ErrorLog.txt", ex.ToString());
+            }
+        }
+
+        public void SendInvoiceEmail(MailMessage mailMessage)
+        {
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+                System.IO.File.WriteAllText(@"c:\ErrorLog.txt", ex.ToString());
+            }
+        }
+
+    }
+}
