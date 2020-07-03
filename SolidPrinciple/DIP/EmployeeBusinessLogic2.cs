79e7ef5723490bace5949915fe8ef302f8bba5b5
git-yeekhaing <yee.khaing.git@gmail.com>
2020-07-03 15:46:21 +0900

    With Dependency Inversion Principle

---
+++ SolidPrinciple/DIP/EmployeeBusinessLogic2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidPrinciple.DIP
+{
+    class EmployeeBusinessLogic2
+    {
+        IEmployeeDataAccess _EmployeeDataAccess;
+        public EmployeeBusinessLogic2()
+        {
+            _EmployeeDataAccess = DataAccessFactory2.GetEmployeeDataAccessObj();
+        }
+
+        public Employee GetEmployeeDetails(int id)
+        {
+            return _EmployeeDataAccess.GetEmployeeDetails(id);
+        }
+    }
+}
